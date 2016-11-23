e1a674e567fde3db505dd0ee86cac77e1ba4e627
chenlin2844 <907169968@qq.com>
2016-11-23 15:38:23 +0800

    完成了onehot编码，label那些向量还没编码，已知问题：随着样本数增加，特征的编码长度越大，还没跑完所有的数据，只跑了100w条

--- read_train_data.py
+++ read_train_data.py
@@ -2,15 +2,23 @@ import csv
 import numpy as np
 from sklearn import preprocessing
 
-def isnumber(num):
-	try:
-		float(num)
-		return True
-	except:
-		return False
+
 
 train_file = '../data/train_ver2.csv'
 test_file = '../data/test_ver2.csv'
+number_col = [5,8,22]
+discard_col = [0,1,6,10,20]
+def isnumber(n):
+	if n in number_col:
+		return True
+	else:
+		return False;
+
+def discard(n):
+	if (n in discard_col) or (n>23):
+		return True
+	else:
+		return False
 
 digit_list=[]
 alpha_list=[]
@@ -21,42 +29,51 @@ with open(train_file,'rb') as csvfile:
 	for i,row in enumerate(spamreader):
 		if i <= 0:
 			continue
-		if i == 10000:
+		if i == 1000000:
 			break
 		row = '|'.join(row)
 		line_list = row.strip().split('|')
 		id_list.append(int(line_list[1]))
 		#print line_list[20]
-		line_list=line_list[2:6]+line_list[7:10]+line_list[11:20]+line_list[21:24]
+		#line_list=line_list[2:6]+line_list[7:10]+line_list[11:20]+line_list[21:24]
 		#print line_list
 		## transform some features to num  or discard the row
-		if line_list[15].strip() == 'NA':# province code transform NA to -1
-			line_list[15] = '-1'
-		if line_list[17].strip() == '' or 'NA': #renta
-			line_list[17] = '-1'
-		if line_list[3].strip() == 'NA': #age if no age ,discard this row
+		if line_list[19].strip() == 'NA':# province code transform NA to -1
+			line_list[19] = '-999999'
+		if (line_list[22].strip() == 'NA') or line_list[22].strip() == '': #renta
+			line_list[22] = '-1'
+		if line_list[5].strip() == 'NA': #age if no age ,discard this row
 			continue
-		line_digit=[float(digit) for digit in line_list if isnumber(digit.strip())==True] #digit
-		line_alpha=[st for st in line_list if isnumber(st.strip())==False] # string
+		if line_list[8].strip() == 'NA': #age if no age ,discard this row
+			line_list[8] = '-1'
+		line_digit=[float(digit.strip()) for n,digit in enumerate(line_list) if (isnumber(n)==True) and discard(n) == False ] #digit
+		line_alpha=[st for n,st in enumerate(line_list) if isnumber(n)==False and discard(n) == False] # string
 		len_gap=len(line_digit) #length of digit line
 		digit_list.append(line_digit)
 		alpha_list.append(line_alpha)
+alpha_arr = np.array(alpha_list)
 
-testarr = np.array(alpha_list)
-#print testarr
-#print (testarr.T)
-
-len_alpha_feature = len(testarr[0])
+len_alpha_feature = len(alpha_arr[0])
 for n in range(len_alpha_feature):
 	le = preprocessing.LabelEncoder()
-	le.fit(testarr.T[n,:])
-	arrle = le.transform(testarr.T[n,:])
-	#print arrle
-	enc = preprocessing.OneHotEncoder()
-	enc.fit(arrle.reshape(-1,1))
-	arrone = enc.transform(arrle.reshape(-1,1))
-	print 'feature ',n
-	print arrone.toarray()
+	le.fit(alpha_arr.T[n,:])
+	arrle = le.transform(alpha_arr.T[n,:])
+	if n == 0:
+		alpha_arr_pro = arrle.reshape(1,-1)
+	else:
+		alpha_arr_pro = np.concatenate((alpha_arr_pro,arrle.reshape(1,-1)),axis = 0)
 	
-#print id_list
 
+enc = preprocessing.OneHotEncoder()
+enc.fit(alpha_arr_pro.T)
+trans_list = enc.transform(alpha_arr_pro.T).toarray()
+
+
+trans_list=np.array(trans_list,dtype=int)
+digit_list=np.array(digit_list,dtype=int)
+fuse_list=np.concatenate([digit_list,trans_list],axis=1)
+
+print fuse_list
+print 'No. of sample: ',len(trans_list)
+print 'length of encoding: ',len(trans_list[0])
+#return fuse_list,id_list,len_gap # no label_list
