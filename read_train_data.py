5e0779b8b95a184f9d253743690cad67d0de1f09
chenlin2844 <907169968@qq.com>
2016-11-24 23:27:20 +0800

    将string类型的特征值编码成整形并存到csv文件中，改变了原文档特征的顺序，连续型数字的特征值放在前几列，其余特征值放在后边

--- read_train_data.py
+++ read_train_data.py
@@ -1,13 +1,14 @@
 import csv
 import numpy as np
 from sklearn import preprocessing
-
+from time import clock
 
 
 train_file = '../data/train_ver2.csv'
 test_file = '../data/test_ver2.csv'
-number_col = [5,8,22]
-discard_col = [0,1,6,10,20]
+
+number_col = [5,8,22] #values in these cols are number  
+discard_col = [0,1,6,10,20] #discard these cols
 def isnumber(n):
 	if n in number_col:
 		return True
@@ -25,11 +26,13 @@ alpha_list=[]
 id_list=[]
 label_list=[]
 with open(train_file,'rb') as csvfile:
+	start = clock()
 	spamreader = csv.reader(csvfile,delimiter=',')
 	for i,row in enumerate(spamreader):
-		if i <= 0:
+		if i == 0:
+			firstline = row
 			continue
-		if i == 1000000:
+		if i == 100000:
 			break
 		row = '|'.join(row)
 		line_list = row.strip().split('|')
@@ -62,7 +65,21 @@ for n in range(len_alpha_feature):
 		alpha_arr_pro = arrle.reshape(1,-1)
 	else:
 		alpha_arr_pro = np.concatenate((alpha_arr_pro,arrle.reshape(1,-1)),axis = 0)
-	
+
+num_code = np.concatenate([digit_list, alpha_arr_pro.T],axis = 1)
+
+feature_name = []
+#change the position of features' name to fit the data
+for col in number_col:
+	feature_name.append(firstline[col])
+for col in range(24):
+	if col not in number_col and col not in discard_col:
+		feature_name.append(firstline[col])
+with open('../data/train_processed.csv','wb') as csvfile:
+	spamwriter = csv.writer(csvfile, delimiter = ',')
+	spamwriter.writerow(feature_name)
+	spamwriter.writerows(num_code)	
+
 
 enc = preprocessing.OneHotEncoder()
 enc.fit(alpha_arr_pro.T)
@@ -73,7 +90,11 @@ trans_list=np.array(trans_list,dtype=int)
 digit_list=np.array(digit_list,dtype=int)
 fuse_list=np.concatenate([digit_list,trans_list],axis=1)
 
-print fuse_list
+#print fuse_list
 print 'No. of sample: ',len(trans_list)
 print 'length of encoding: ',len(trans_list[0])
 #return fuse_list,id_list,len_gap # no label_list
+
+
+finish = clock()
+print 'time: ',(finish - start),' s'
